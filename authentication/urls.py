dba51d3c630e2a258b5b911676a7f7171dbdeecf
shikun chen <shikun.chen@stud.uni-due.de>
2021-06-08 22:55:43 +0200

    add register for email and verify it

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import EmailValidationView, RegistrationView, UsernameValidationView
+from .views import (
+    EmailValidationView, 
+    RegistrationView, 
+    UsernameValidationView, 
+    VerificationView)
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name="register"),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name="validate-email"),
+    path('activate/<uidb64>/<token>', VerificationView.as_view(), name="activate")
 ]
