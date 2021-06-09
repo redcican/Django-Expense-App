660f294cc164dbabfa32769832f42ceb5e9221be
shikun chen <shikun.chen@stud.uni-due.de>
2021-06-09 21:56:21 +0200

    add user preferences

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -1,5 +1,40 @@
+from . models import UserPreference
 from django.shortcuts import render
+from django.conf import settings
+import json
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'preferences/index.html')
+    currency_data = []
+        
+    file_path = settings.BASE_DIR / 'currencies.json'
+        
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+        for key, value in data.items():
+            currency_data.append({'name': key, 'value': value})
+            
+    exists = UserPreference.objects.filter(user=request.user).exists()
+    user_preferences = None
+    
+    if exists:
+        user_preferences = UserPreference.objects.get(user=request.user)
+        
+    if request.method == 'GET':
+        # import pdb
+        # pdb.set_trace()
+        return render(request, 'preferences/index.html',{'currencies':currency_data, 'user_preferences': user_preferences})
+    
+    else:  # method=='POST'
+        currency = request.POST['currency_obj']
+        if exists:
+            user_preferences.currency = currency
+            user_preferences.save()
+        else:
+            UserPreference.objects.create(user=request.user, currency=currency)
+        messages.success(request, 'Changes saved!')
+        return render(request, 'preferences/index.html',{'currencies':currency_data,'user_preferences': user_preferences})
+ 
+        # import pdb
+        # pdb.set_trace()
