c0a4a9d7f5b889d34324979980c4be2bc96dc680
shikun chen <shikun.chen@stud.uni-due.de>
2021-06-09 20:22:23 +0200

    add new app

--- expensenswebsite/urls.py
+++ expensenswebsite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('expenses.urls')),
     path('authentication/', include('authentication.urls')),
     path('admin/', admin.site.urls),
+    path('preferences/', include('userpreferences.urls'))
 ]
