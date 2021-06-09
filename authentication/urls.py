28e75694311509d5e7b7debc1aa8ae15935117a4
Mohammad Armoun <Mohammad Armoun@DESKTOP-J2KIK2H>
2021-06-09 13:05:10 +0200

    add login veiw and templates

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
     EmailValidationView,
-    LoginView, 
+    LoginView,
+    LogoutView, 
     RegistrationView, 
     UsernameValidationView, 
     VerificationView)
@@ -10,6 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('register', RegistrationView.as_view(), name="register"),
     path('login', LoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name="validate-email"),
     path('activate/<uidb64>/<token>', VerificationView.as_view(), name="activate")
