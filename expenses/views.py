6d0ac389f5a73fa68589cc22e0501320437ed290
shikun chen <shikun.chen@stud.uni-due.de>
2021-06-10 22:18:13 +0200

    add search function for expenses

--- expenses/views.py
+++ expenses/views.py
@@ -4,6 +4,24 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
 # Create your views here.
+import json
+from django.http import JsonResponse
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner = request.user
+        ) | Expense.objects.filter(
+            date__istartswith=search_str, owner = request.user
+        ) | Expense.objects.filter(
+            description__icontains=search_str, owner = request.user
+        ) | Expense.objects.filter(
+            category__icontains=search_str, owner = request.user
+        )
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 @login_required(login_url="/authentication/login")
 def index(request):
@@ -11,7 +29,7 @@ def index(request):
     
     # retrieve the expense
     expenses = Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses, 2)
+    paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
